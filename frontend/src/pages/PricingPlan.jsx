0aefa8be7f365d9f0b426dfb3267440a0f5b2332
Abdul Wahab <aws18320@gmail.com>
2024-02-26 11:29:36 +0500

    Refactored Plans Routing

--- frontend/src/pages/PricingPlan.jsx
+++ frontend/src/pages/PricingPlan.jsx
@@ -1,5 +1,6 @@
 import Pricing from '../components/common/Pricing';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const plans = [
 	{
@@ -21,6 +22,11 @@ export const plans = [
 
 const PricingPlan = () => {
 	const [selectedPlan, setSelectedPlan] = useState(null);
+	const navigate = useNavigate();
+
+	const handlePlanSelection = () => {
+		navigate('/dashboard');
+	};
 
 	return (
 		<div className="w-full min-h-screen p-10 flex flex-col gap-12 auth-wrapper">
@@ -32,6 +38,7 @@ const PricingPlan = () => {
 			<button
 				disabled={!selectedPlan}
 				className={`${selectedPlan ? 'gradient-bg' : 'gradient-border'}  text-lg rounded-xl text-white  mx-auto py-2 px-36 `}
+				onClick={handlePlanSelection}
 			>
 				Next
 			</button>
