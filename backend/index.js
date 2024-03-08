faed8f09282ff0145f3ae334d0cadb6fac1dc8f3
Abdul Wahab <aws18320@gmail.com>
2024-03-08 21:24:16 +0500

    Refactored Cors code

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,22 @@ const cookieParser = require('cookie-parser');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
 const cors = require('cors');
 
-app.use(cors());
+const corsOpts = {
+	origin: 'https://glistening-sprite-30779a.netlify.app/',
+
+	methods: ['GET', 'POST'],
+
+	allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOpts));
+
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'https://glistening-sprite-30779a.netlify.app/');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
