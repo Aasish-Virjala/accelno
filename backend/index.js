a76de2304690db15515a0085f4550e30747d9d3c
Abdul Wahab <aws18320@gmail.com>
2024-03-08 21:34:15 +0500

    Added res.header to allow requests with credentials

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', 'https://glistening-sprite-30779a.netlify.app/');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.header('Access-Control-Allow-Credentials', true);
 	next();
 });
 
